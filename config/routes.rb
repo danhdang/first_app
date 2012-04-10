b708593c8ec4c3d28883580155c280a5ba300ac2
Danh Dang <danh.dang@thomonreuters.com>
2012-04-10 19:16:55 +0200

    add code

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 FirstApp::Application.routes.draw do
+  resources :posts do
+    resources :comments
+  end
+   
+
+  get "home/index"
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
